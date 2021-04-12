360f56a0be8c5c26762447810f515b4b7c6907dd
dhananjayind <80812387+dhananjayind@users.noreply.github.com>
2021-04-12 18:13:39 +0530

    Update TestStart.java

--- src/main/java/RunTest/TestStart.java
+++ src/main/java/RunTest/TestStart.java
@@ -1,23 +1,13 @@
 package RunTest;
 
-
-
-
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
-
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "C:\\Users\\JAY\\eclipse-workspace\\MailCompose\\src\\main\\java\\Feature",
-		glue={"MailComposeSteps"}, //the path of the step definition files
-				plugin= {"pretty","html:test-outout"}		
-		)
-
-
+@CucumberOptions(features = "C:\\Users\\JAY\\eclipse-workspace\\MailCompose\\src\\main\\java\\Feature", glue = {
+		"MailComposeSteps" }, plugin = { "pretty", "html:test-outout" })
 
 public class TestStart {
 
