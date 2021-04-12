06a28b80e15d589bea85c11fd4de684020c145f7
dhananjayind <80812387+dhananjayind@users.noreply.github.com>
2021-04-12 18:14:35 +0530

    Update MainUI.java

--- src/main/java/UserInterface/MainUI.java
+++ src/main/java/UserInterface/MainUI.java
@@ -14,47 +14,39 @@ public class MainUI {
 
 	public WebDriver driver;
 	public Properties property;
-	
+
 	public void invokeBrowser(String browserName) {
 
-	
-			if (browserName.equalsIgnoreCase("Chrome")) {
-				System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\main\\java\\resources\\chromedriver.exe");
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\chromedriver.exe");
 
-				driver = new ChromeDriver();
-			} else if (browserName.equalsIgnoreCase("Mozila")) {
-				System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir") + "\\src\\\\main\\\\java\\\\resources\\geckodriver.exe");
-				driver = new FirefoxDriver();
-			} else {
-				System.setProperty("webdriver.opera.driver",System.getProperty("user.dir") + "\\src\\main\\java\\resources\\operadriver.exe");
-				driver = new OperaDriver();
-			}
-		 
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("Mozila")) {
+			System.setProperty("webdriver.gecko.driver",
+					System.getProperty("user.dir") + "\\src\\\\main\\\\java\\\\resources\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			System.setProperty("webdriver.opera.driver",
+					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\operadriver.exe");
+			driver = new OperaDriver();
+		}
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 
-		
 	}
-	
-	/* =================Open URL================== */
+
+	// =================Open URL==================
 	public void openURL() {
-			
+
 		driver.get("https://www.rediff.com/");
 	}
-	
 
-	
-	/* =================Closing Browser================== */
-	public void tearBrowser() {
-		driver.close();
-	}
-
-	/* =================Closing Browser Instance================== */
+	// =================Closing Browser Instance===
 	public void quitBrowser() {
 		driver.quit();
 	}
-	
-	
+
 }
